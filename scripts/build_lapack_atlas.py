376d774f4208577936ba977ea4d97602e979e699
Matthew Brett <matthew.brett@gmail.com>
2009-08-06 13:49:30 -0700

    URL download too

--- scripts/build_lapack_atlas.py
+++ scripts/build_lapack_atlas.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-''' Script to build LAPACK locally
+''' Script to build LAPACK and ATLAS locally
 
 ftp://ftp.netlib.org/lapack/
 
@@ -8,6 +8,8 @@ from __future__ import with_statement
 
 import os
 from os.path import join as pjoin
+from urllib import urlretrieve
+
 from atlastools import lapack, atlas, archive_version
 from atlastools import get_cpuinfo
 
@@ -17,15 +19,20 @@ HOME = os.environ['HOME']
 # Change these to suit your setup
 SOURCE_ROOT = pjoin(HOME, 'stable_trees', 'atlas')
 ARCHIVE_DIR = pjoin(SOURCE_ROOT, 'archives')
-LAPACK_ARCHIVE = pjoin(ARCHIVE_DIR, 'lapack-3.2.1.tgz')
-ATLAS_ARCHIVE = pjoin(ARCHIVE_DIR, 'atlas3.9.11.tar.bz2')
+LAPACK_FILE = 'lapack-3.2.1.tgz'
+ATLAS_FILE = 'atlas3.9.11.tar.bz2'
 BUILD_TYPE = 64 # one of 64 or 32
 COMPILE_FLAGS = '-m%s -fPIC -msse3' % BUILD_TYPE
 PLATFORM = 'X64_SSE3'
 
 # Change these if you like, but they should take care of themselves
+LAPACK_ARCHIVE = pjoin(ARCHIVE_DIR, LAPACK_FILE)
 LAPACK_DIR = pjoin(SOURCE_ROOT, 'lapack-%s' % archive_version(LAPACK_ARCHIVE))
-ATLAS_DIR = pjoin(SOURCE_ROOT, 'atlas-%s' % archive_version(ATLAS_ARCHIVE))
+LAPACK_URL = pjoin('ftp://ftp.lapack.org/lapack', LAPACK_FILE)
+ATLAS_ARCHIVE = pjoin(ARCHIVE_DIR, ATLAS_FILE)
+_atlas_version = archive_version(ATLAS_ARCHIVE)
+ATLAS_DIR = pjoin(SOURCE_ROOT, 'atlas-%s' % _atlas_version
+ATLAS_URL = pjoin('http://downloads.sourceforge.net/project/math-atlas/Developer%20%28unstable%29/%s' % _atlas_version, ATLAS_FILE)
 LAPACK_OPTS = {'PLAT': '_' + PLATFORM,
         'OPTS': '-O2 ' + COMPILE_FLAGS,
         'NOOPT': '-O0 ' + COMPILE_FLAGS}
@@ -39,7 +46,9 @@ ATLAS_FLAGS = ('-b %(BUILD_TYPE)s ' +
 
 
 def main():
+    urlretrieve(LAPACK_URL, LAPACK_ARCHIVE)
     lapack.build(LAPACK_ARCHIVE, LAPACK_DIR, LAPACK_OPTS)
+    urlretrieve(ATLAS_URL, ATLAS_ARCHIVE)
     atlas.build_in(ATLAS_ARCHIVE, ATLAS_DIR, ATLAS_BUILD_DIR, ATLAS_FLAGS)
     
 
